b493f7d209d241055eb0eab3de32e09e84178fea
Thai Nguyen <thainv299@gmail.com>
2026-04-04 21:05:08 +0700

    feat: clean up system execution scripts

--- exportRT.py
+++ exportRT.py
@@ -3,7 +3,8 @@ from ultralytics import YOLO
 model = YOLO("models/best.pt")
 model.export(
     format="engine",
-    half=True,        # FP16 engine
+    half=True,        
     device=0,
-    workspace=4       # GB VRAM dành cho TensorRT
+    workspace=4       
 )
+
